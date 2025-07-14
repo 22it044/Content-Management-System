eb90f2bf0de2f088ef4ac5abf13702043aff7bda
Shrivardhansinh Jadeja <22it044@charusat.edu.in>
2025-07-14 11:00:43 +0530

    Helper functions for the project

---
+++ helper.php
@@ -0,0 +1,29 @@
+<?php
+function base_url($path="") {
+    $protocol = isset($_SERVER['HTTPS'] ) && $_SERVER['HTTPS'] !== 'off' ? 'https://' : 'http://';
+    $host = $_SERVER['HTTP_HOST'];
+    $base_url =  $protocol . $host;
+    return $base_url . '/' . ltrim($path,'/');
+}
+
+function base_path($path=""){
+    $root_path = dirname(__DIR__);
+    $base_path = $root_path . DIRECTORY_SEPARATOR . ltrim($path, DIRECTORY_SEPARATOR);
+}
+
+function uploads_path($filename=''){
+    return base_path('uploads' . DIRECTORY_SEPARATOR . $filename);
+}
+
+function uploads_url($filename=''){
+    return base_path('uploads/' . ltrim( $filename, '/'));
+}
+
+function asset_url ($path = ''){
+    return base_url('assets/') . ltrim($path, '/');
+}
+
+function redirect($url){
+    header('Location: ' . base_url($url));
+    exit;
+}
